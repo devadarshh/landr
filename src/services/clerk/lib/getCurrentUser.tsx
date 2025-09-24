e9bac8acb25ea235ae42a1a9bf8c6e1662ad4175
devadarshh <imadarshsingh2002@gmail.com>
2025-09-24 20:35:40 +0530

    fix : prisma vercel error

--- src/services/clerk/lib/getCurrentUser.tsx
+++ src/services/clerk/lib/getCurrentUser.tsx
@@ -1,17 +1,6 @@
 import { auth } from "@clerk/nextjs/server";
 import { prisma } from "../../../lib/prisma";
 
-async function getUser(id: string) {
-  try {
-    return await prisma.user.findUnique({
-      where: { id },
-    });
-  } catch (error) {
-    console.error("Prisma error:", error);
-    return null;
-  }
-}
-
 export async function getCurrentUser({ allData = false } = {}) {
   const { userId, redirectToSignIn } = await auth();
 
@@ -19,14 +8,17 @@ export async function getCurrentUser({ allData = false } = {}) {
     return { userId: null, redirectToSignIn, user: undefined };
   }
 
-  let user;
-  if (allData) {
-    try {
-      user = await getUser(userId);
-    } catch (e) {
-      user = undefined;
-    }
+  return {
+    userId,
+    redirectToSignIn,
+    user: allData ? await getUser(userId) : undefined,
+  };
+}
+async function getUser(id: string) {
+  try {
+    return await prisma.user.findUnique({ where: { id } });
+  } catch (err) {
+    console.error("DB connection error:", err);
+    return null;
   }
-
-  return { userId, redirectToSignIn, user };
 }
